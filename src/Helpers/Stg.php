bf2d68a2dff087139feb47d9048ba157b48a9d6e
Tirdad <abbasi.tirdad@gmail.com>
2020-04-26 04:08:29 +0430

    Helper class development

---
+++ src/Helpers/Stg.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tir\Setting\Helpers;
+
+class Stg
+{
+    /**
+     * Get / set the specified setting value.
+     *
+     * If an array is passed, we'll assume you want to set settings.
+     *
+     * @param string|array $key
+     * @param mixed $default
+     * @return mixed|\Modules\Setting\Repository
+     */
+    public static function get($key = null, $default = null)
+    {
+        return false;
+        if (is_null($key)) {
+            return app('setting');
+        }
+
+        if (is_array($key)) {
+            return app('setting')->set($key);
+        }
+
+        try {
+            return app('setting')->get($key, $default);
+        } catch (PDOException $e) {
+            return $default;
+        }
+    }
+}
---
+++ src/SettingServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tir\Setting;
+
+
+use Illuminate\Support\ServiceProvider;
+
+
+class SettingServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+
+    public function register()
+    {
+
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+    }
+}
