0c935936492407de2b2b3d4b80fb7ae18da5b28f
Tirdad <abbasi.tirdad@gmail.com>
2020-06-24 19:31:16 +0430

    Add StoreSetting module and remove settings from settings package

--- src/Routes/admin.php
+++ src/Routes/admin.php
@@ -7,8 +7,8 @@ Route::group(['middleware' => 'web'], function () {
     //add admin prefix and middleware for admin area to user module
     Route::group(['prefix' => 'admin', 'middleware' => 'IsAdmin'], function () {
         // Route::resource('/setting', 'Tir\Setting\Controllers\AdminSettingController');
-        Route::get('/setting', 'Tir\Setting\Controllers\AdminSettingController@editSetting')->name('setting.edit');
-        Route::get('/setting', 'Tir\Setting\Controllers\AdminSettingController@editSetting')->name('setting.index');
+//        Route::get('/setting', 'Tir\Setting\Controllers\AdminSettingController@editSetting')->name('setting.edit');
+//        Route::get('/setting', 'Tir\Setting\Controllers\AdminSettingController@editSetting')->name('setting.index');
         Route::put('/setting', 'Tir\Setting\Controllers\AdminSettingController@updateSetting')->name('setting.update');
     });
 
